41e9c0390e8b8426d1dbed5d7b023728342aa141
Alex Orlenko <zxteam@protonmail.com>
2025-04-29 10:07:05 +0100

    Refactor build rules Remove include_dir from artifacts

--- src/lib.rs
+++ src/lib.rs
@@ -14,23 +14,25 @@ pub use self::Version::*;
 pub struct Build {
     out_dir: Option<PathBuf>,
     target: Option<String>,
-    host: Option<String>,
 }
 
 pub struct Artifacts {
-    include_dir: PathBuf,
     lib_dir: PathBuf,
     libs: Vec<String>,
 }
 
-impl Build {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> Build {
+impl Default for Build {
+    fn default() -> Build {
         Build {
-            out_dir: env::var_os("OUT_DIR").map(|s| PathBuf::from(s).join("lua-build")),
+            out_dir: env::var_os("OUT_DIR").map(PathBuf::from),
             target: env::var("TARGET").ok(),
-            host: env::var("HOST").ok(),
         }
+    }
+}
+
+impl Build {
+    pub fn new() -> Build {
+        Build::default()
     }
 
     pub fn out_dir<P: AsRef<Path>>(&mut self, path: P) -> &mut Build {
@@ -43,43 +45,15 @@ impl Build {
         self
     }
 
-    pub fn host(&mut self, host: &str) -> &mut Build {
-        self.host = Some(host.to_string());
-        self
-    }
-
     pub fn build(&mut self, version: Version) -> Artifacts {
-        let target = &self.target.as_ref().expect("TARGET not set")[..];
-        let host = &self.host.as_ref().expect("HOST not set")[..];
-        let out_dir = self.out_dir.as_ref().expect("OUT_DIR not set");
-        let lib_dir = out_dir.join("lib");
-        let include_dir = out_dir.join("include");
-
-        let source_dir_base = Path::new(env!("CARGO_MANIFEST_DIR"));
-        let mut source_dir = match version {
-            Lua51 => source_dir_base.join("lua-5.1.5"),
-            Lua52 => source_dir_base.join("lua-5.2.4"),
-            Lua53 => source_dir_base.join("lua-5.3.6"),
-            Lua54 => source_dir_base.join("lua-5.4.7"),
-        };
-
-        if lib_dir.exists() {
-            fs::remove_dir_all(&lib_dir).unwrap();
-        }
-        fs::create_dir_all(&lib_dir).unwrap();
-
-        if include_dir.exists() {
-            fs::remove_dir_all(&include_dir).unwrap();
-        }
-        fs::create_dir_all(&include_dir).unwrap();
+        let target = &self.target.as_ref().expect("TARGET is not set")[..];
+        let out_dir = self.out_dir.as_ref().expect("OUT_DIR is not set");
+        let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let mut source_dir = manifest_dir.join(version.source_dir());
+        let build_dir = out_dir.join("lua-build");
 
         let mut config = cc::Build::new();
-        config
-            .target(target)
-            .host(host)
-            .warnings(false)
-            .opt_level(2)
-            .cargo_metadata(false);
+        config.warnings(false).cargo_metadata(false);
 
         match target {
             _ if target.contains("linux") => {
@@ -132,8 +106,8 @@ impl Build {
                 }
                 source_dir = cpp_source_dir
             }
-            _ => panic!("don't know how to build Lua for {}", target),
-        };
+            _ => panic!("don't know how to build Lua for {target}"),
+        }
 
         if let Lua54 = version {
             config.define("LUA_COMPAT_5_3", None);
@@ -145,93 +119,42 @@ impl Build {
             config.define("LUA_USE_APICHECK", None);
         }
 
-        let lib_name = match version {
-            Lua51 => "lua5.1",
-            Lua52 => "lua5.2",
-            Lua53 => "lua5.3",
-            Lua54 => "lua5.4",
-        };
-
         config
             .include(&source_dir)
             .flag("-w") // Suppress all warnings
             .flag_if_supported("-fno-common") // Compile common globals like normal definitions
-            .file(source_dir.join("lapi.c"))
-            .file(source_dir.join("lauxlib.c"))
-            .file(source_dir.join("lbaselib.c"))
-            // skipped: lbitlib.c (>= 5.2, <= 5.3)
-            .file(source_dir.join("lcode.c"))
-            // skipped: lcorolib.c (>= 5.2)
-            // skipped: lctype.c (>= 5.2)
-            .file(source_dir.join("ldblib.c"))
-            .file(source_dir.join("ldebug.c"))
-            .file(source_dir.join("ldo.c"))
-            .file(source_dir.join("ldump.c"))
-            .file(source_dir.join("lfunc.c"))
-            .file(source_dir.join("lgc.c"))
-            .file(source_dir.join("linit.c"))
-            .file(source_dir.join("liolib.c"))
-            .file(source_dir.join("llex.c"))
-            .file(source_dir.join("lmathlib.c"))
-            .file(source_dir.join("lmem.c"))
-            .file(source_dir.join("loadlib.c"))
-            .file(source_dir.join("lobject.c"))
-            .file(source_dir.join("lopcodes.c"))
-            .file(source_dir.join("loslib.c"))
-            .file(source_dir.join("lparser.c"))
-            .file(source_dir.join("lstate.c"))
-            .file(source_dir.join("lstring.c"))
-            .file(source_dir.join("lstrlib.c"))
-            .file(source_dir.join("ltable.c"))
-            .file(source_dir.join("ltablib.c"))
-            .file(source_dir.join("ltm.c"))
-            .file(source_dir.join("lundump.c"))
-            // skipped: lutf8lib.c (>= 5.3)
-            .file(source_dir.join("lvm.c"))
-            .file(source_dir.join("lzio.c"));
-
-        match version {
-            Lua51 => {}
-            Lua52 => {
-                config
-                    .file(source_dir.join("lbitlib.c"))
-                    .file(source_dir.join("lcorolib.c"))
-                    .file(source_dir.join("lctype.c"));
-            }
-            Lua53 => {
-                config
-                    .file(source_dir.join("lbitlib.c"))
-                    .file(source_dir.join("lcorolib.c"))
-                    .file(source_dir.join("lctype.c"))
-                    .file(source_dir.join("lutf8lib.c"));
-            }
-            Lua54 => {
-                config
-                    .file(source_dir.join("lcorolib.c"))
-                    .file(source_dir.join("lctype.c"))
-                    .file(source_dir.join("lutf8lib.c"));
-            }
-        }
-
-        config.out_dir(&lib_dir).compile(lib_name);
-
-        for f in &["lauxlib.h", "lua.h", "luaconf.h", "lualib.h"] {
-            fs::copy(source_dir.join(f), include_dir.join(f)).unwrap();
-        }
+            .add_files_by_ext(&source_dir, "c")
+            .out_dir(&build_dir)
+            .compile(version.lib_name());
 
         Artifacts {
-            lib_dir,
-            include_dir,
-            libs: vec![lib_name.to_string()],
+            lib_dir: build_dir,
+            libs: vec![version.lib_name().to_string()],
+        }
+    }
+}
+
+impl Version {
+    fn source_dir(&self) -> &str {
+        match self {
+            Lua51 => "lua-5.1.5",
+            Lua52 => "lua-5.2.4",
+            Lua53 => "lua-5.3.6",
+            Lua54 => "lua-5.4.7",
+        }
+    }
+
+    fn lib_name(&self) -> &str {
+        match self {
+            Lua51 => "lua5.1",
+            Lua52 => "lua5.2",
+            Lua53 => "lua5.3",
+            Lua54 => "lua5.4",
         }
     }
 }
 
 impl Artifacts {
-    pub fn include_dir(&self) -> &Path {
-        &self.include_dir
-    }
-
     pub fn lib_dir(&self) -> &Path {
         &self.lib_dir
     }
@@ -243,9 +166,24 @@ impl Artifacts {
     pub fn print_cargo_metadata(&self) {
         println!("cargo:rustc-link-search=native={}", self.lib_dir.display());
         for lib in self.libs.iter() {
-            println!("cargo:rustc-link-lib=static={}", lib);
+            println!("cargo:rustc-link-lib=static={lib}");
         }
-        println!("cargo:include={}", self.include_dir.display());
-        println!("cargo:lib={}", self.lib_dir.display());
+    }
+}
+
+trait AddFilesByExt {
+    fn add_files_by_ext(&mut self, dir: &Path, ext: &str) -> &mut Self;
+}
+
+impl AddFilesByExt for cc::Build {
+    fn add_files_by_ext(&mut self, dir: &Path, ext: &str) -> &mut Self {
+        for entry in fs::read_dir(dir)
+            .unwrap()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.path().extension() == Some(ext.as_ref()))
+        {
+            self.file(entry.path());
+        }
+        self
     }
 }
--- testcrate/src/lib.rs
+++ testcrate/src/lib.rs
@@ -56,66 +56,72 @@ pub unsafe fn lua_pcall(
     lua_pcallk(state, nargs, nresults, errfunc, 0, std::ptr::null())
 }
 
-#[test]
-fn test_lua() {
-    use std::{ptr, slice};
-    unsafe {
-        let state = luaL_newstate();
-        assert!(state != ptr::null_mut());
-
-        luaL_openlibs(state);
-
-        let version = {
-            lua_getglobal(state, "_VERSION\0".as_ptr().cast());
-            let mut len: c_long = 0;
-            let version_ptr = lua_tolstring(state, -1, &mut len);
-            slice::from_raw_parts(version_ptr as *const u8, len as usize)
-        };
-
-        #[cfg(feature = "lua51")]
-        assert_eq!(version, "Lua 5.1".as_bytes());
-        #[cfg(feature = "lua52")]
-        assert_eq!(version, "Lua 5.2".as_bytes());
-        #[cfg(feature = "lua53")]
-        assert_eq!(version, "Lua 5.3".as_bytes());
-        #[cfg(feature = "lua54")]
-        assert_eq!(version, "Lua 5.4".as_bytes());
-    }
+pub unsafe fn to_string<'a>(state: *mut c_void, index: c_int) -> &'a str {
+    let mut len: c_long = 0;
+    let ptr = lua_tolstring(state, index, &mut len);
+    let bytes = std::slice::from_raw_parts(ptr as *const u8, len as usize);
+    std::str::from_utf8(bytes).unwrap()
 }
 
-#[test]
-fn test_unicode_identifiers() {
-    unsafe {
-        let state = luaL_newstate();
-        let code = "local 😀 = 0\0";
-        let ret = luaL_loadstring(state, code.as_ptr().cast());
-        #[cfg(feature = "lua54")]
-        assert_eq!(0, ret);
-        #[cfg(not(feature = "lua54"))]
-        assert_ne!(0, ret);
-    }
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_exceptions() {
-    use std::{ptr, slice, str};
-    unsafe {
-        let state = luaL_newstate();
-        assert!(state != ptr::null_mut());
+    #[test]
+    fn test_lua_version() {
+        unsafe {
+            let state = luaL_newstate();
+            assert!(!state.is_null());
 
-        unsafe extern "C-unwind" fn it_panics(state: *mut c_void) -> c_int {
-            luaL_error(state, "exception!\0".as_ptr().cast())
+            luaL_openlibs(state);
+
+            lua_getglobal(state, c"_VERSION".as_ptr());
+            let version = to_string(state, -1);
+
+            #[cfg(feature = "lua51")]
+            assert_eq!(version, "Lua 5.1");
+            #[cfg(feature = "lua52")]
+            assert_eq!(version, "Lua 5.2");
+            #[cfg(feature = "lua53")]
+            assert_eq!(version, "Lua 5.3");
+            #[cfg(feature = "lua54")]
+            assert_eq!(version, "Lua 5.4");
         }
+    }
 
-        lua_pushcclosure(state, it_panics, 0);
-        let result = lua_pcall(state, 0, 0, 0);
-        assert_eq!(result, 2); // LUA_ERRRUN
-        let s = {
-            let mut len: c_long = 0;
-            let version_ptr = lua_tolstring(state, -1, &mut len);
-            let s = slice::from_raw_parts(version_ptr as *const u8, len as usize);
-            str::from_utf8(s).unwrap()
-        };
-        assert_eq!(s, "exception!");
+    #[test]
+    fn test_unicode_identifiers() {
+        unsafe {
+            let state = luaL_newstate();
+            let ret = luaL_loadstring(state, c"😀 = '🌚︎'".as_ptr());
+
+            #[cfg(feature = "lua54")]
+            {
+                assert_eq!(ret, 0);
+                assert_eq!(lua_pcall(state, 0, 0, 0), 0);
+                lua_getglobal(state, c"😀".as_ptr());
+                assert_eq!(to_string(state, -1), "🌚︎");
+            }
+
+            #[cfg(not(feature = "lua54"))]
+            assert_ne!(ret, 0);
+        }
+    }
+
+    #[test]
+    fn test_exceptions() {
+        unsafe {
+            let state = luaL_newstate();
+            assert!(!state.is_null());
+
+            unsafe extern "C-unwind" fn it_panics(state: *mut c_void) -> c_int {
+                luaL_error(state, c"exception!".as_ptr())
+            }
+
+            lua_pushcclosure(state, it_panics, 0);
+            let result = lua_pcall(state, 0, 0, 0);
+            assert_eq!(result, 2); // LUA_ERRRUN
+            assert_eq!(to_string(state, -1), "exception!");
+        }
     }
 }
